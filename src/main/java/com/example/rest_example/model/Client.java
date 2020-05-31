18e6cf4e81498d41bb56fa4d8f9a3a849d499ed8
tonyrmk <anton.rymarenko@gmail.com>
2020-05-31 16:04:55 +0300

    Add dataBase PostgreSQL with update

--- src/main/java/com/example/rest_example/model/Client.java
+++ src/main/java/com/example/rest_example/model/Client.java
@@ -1,9 +1,27 @@
 package com.example.rest_example.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "clients")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Client {
+
+    @Id
+    @Column(name="id")
+    @SequenceGenerator(name = "clientsIdSeq", sequenceName = "clients_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "clientsIdSeq")
     private Integer id;
+
+    @Column(name ="name")
     private String name ;
+
+    @Column(name ="email")
     private String email;
+
+    @Column (name ="phone")
     private String phone;
 
     public Integer getId() {
--- src/main/java/com/example/rest_example/service/ClientServiceImpl.java
+++ src/main/java/com/example/rest_example/service/ClientServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.rest_example.service;
 
 import com.example.rest_example.model.Client;
+import com.example.rest_example.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -12,32 +14,33 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Service
 public class ClientServiceImpl implements ClientService{
 
-    private static final Map<Integer ,Client> CLIENT_REPOSITORY_MAP =new HashMap<>();
+   // private static final Map<Integer ,Client> CLIENT_REPOSITORY_MAP =new HashMap<>();
 
-    private static final AtomicInteger CLIENT_ID_HOLDER =new AtomicInteger();
+   // private static final AtomicInteger CLIENT_ID_HOLDER =new AtomicInteger();
+
+    @Autowired
+    ClientRepository clientRepository;
 
     @Override
     public void createClient(Client client) {
-    final int clientId = CLIENT_ID_HOLDER.incrementAndGet();
-    client.setId(clientId);
-    CLIENT_REPOSITORY_MAP.put(clientId ,client) ;
+    clientRepository.save(client);
     }
 
     @Override
     public List<Client> readAll() {
-        return new ArrayList<>(CLIENT_REPOSITORY_MAP.values());
+        return clientRepository.findAll();
     }
 
     @Override
     public Client read(int id) {
-        return CLIENT_REPOSITORY_MAP.get(id);
+        return clientRepository.getOne(id);
     }
 
     @Override
     public boolean update(Client client, int id) {
-        if(CLIENT_REPOSITORY_MAP.containsKey(id)){
+        if(clientRepository.existsById(id)){
             client.setId(id);
-            CLIENT_REPOSITORY_MAP.put(id ,client);
+            clientRepository.save(client);
             return true;
         }
         return false;
@@ -45,6 +48,10 @@ public class ClientServiceImpl implements ClientService{
 
     @Override
     public boolean delete(int id) {
-        return CLIENT_REPOSITORY_MAP.remove(id)!=null;
+        if(clientRepository.existsById(id)){
+            clientRepository.deleteById(id);
+            return true;
+        }
+        return false;
     }
 }
